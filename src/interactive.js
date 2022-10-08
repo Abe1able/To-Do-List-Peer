c6997ba9cfddd37f6ec18da8329452ba986266f6
Abel Seifu <abel.workmail@gmail.com>
2022-10-08 12:18:11 +0300

    Improve code as per review comments

--- src/interactive.js
+++ src/interactive.js
@@ -1,9 +1,7 @@
 export default function interactive(e) {
     if (e.target.checked) {
       e.target.nextElementSibling.classList.add('complete');
-      console.log("complete");
     } else {
       e.target.nextElementSibling.classList.remove('complete');
-      console.log("incomplete");
     }
   }
